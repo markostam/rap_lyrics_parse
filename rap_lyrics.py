ae9949cb9c0c2c9b6352338c2ed81f6e51f75119
markostam <markostam@gmail.com>
2016-05-10 04:54:23 -0400

    cleaning up lyrics scraped from rapgenius and azlrics. mainly separating the featured artists into their own entries for disambiguation

--- rap_lyrics.py
+++ rap_lyrics.py
@@ -19,86 +19,73 @@ from fuzzywuzzy import process
 
 rg = '/Users/markostamenovic/GoogleDrive/Education/UR Spring 2016/Linguistics for Data Science - CSC/Final Project/rg_local_final.json'
 az = '/Users/markostamenovic/GoogleDrive/Education/UR Spring 2016/Linguistics for Data Science - CSC/Final Project/lyrics_az.json'
-
-rg = pd.read_json(rg)
-#clear out nonrap and nonelnglish songs
-rg_rap = rg[rg['genre']=='rap'] #get rid of the nonrap
-rg_rap = rg_rap[rg_rap['year'] != 'N/A'] #clean out undated rap
-rg_rap['year']=rg_rap['year'].apply(lambda x: x.replace('}','')) #clean it up
-rg_rap['datetime']=rg_rap['year'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d')) #convert string date to dateime
-rg_rap['year']=rg_rap['datetime'].apply(lambda x: x.year) #put year in it's own column for simplicity
-rg_rap = rg_rap[rg_rap['year'] > 1979] #stuff before 1979 isn't rap it's all james brown etc
-rg_rap['lang'] = rg_rap['lyrics'].apply(lambda x: langDetect(x)) #get english songs
-rg_rap = rg_rap[rg_rap['lang']=='en'] #banish nonenglish songs
-#downcase
-rg_rap['artist'] = rg_rap['artist'].apply(lambda x: x.lower())
-rg_rap['title'] = rg_rap['title'].apply(lambda x: x.lower())
-rg_rap['album'] = rg_rap['album'].apply(lambda x: x.lower())
-rg_rap['artist'] = rg_rap['artist'].apply(lambda x: x.lower())
-rg_rap['producer'] = rg_rap['producer'].apply(lambda x: [i.lower() for i in x])
-rg_rap['producer'] = rg_rap['producer'].apply(lambda x: [i.lower() for i in x])
-rg_rap['featured_artist'] = rg_rap['featured_artist'].apply(lambda x: [i.lower() for i in x])
-rg_rap['writer'] = rg_rap['writer'].apply(lambda x: [i.lower() for i in x])
-rg_rap['lyrics'] = rg_rap['lyrics'].apply(lambda x: [i.lower() for i in x])
-#remove ' lyrics' from song title
-rg_rap['title'] = rg_rap['title'].apply(lambda x: x.replace(' lyrics',''))
-#parse all collaborators from lyric data
-[re.findall(pattern,i) for i in [j[0] for j in a] if re.findall(pattern,i)]
-#set of all collaborators from lyric data
-a=[i for i in rg_rap['lyrics']]
-len(set([k[0] for k in [re.findall(pattern,i) for i in [j[0] for j in a] if re.findall(pattern,i)]]))
-#TODO
-#also have to remove '2X' and spaces at EOL
-#convert &amp; to &
-#get all collaborators who appear more than once. these are good to go for rearranging
-b=Counter([k[0] for k in [re.findall(pattern,i) for i in [j[0] for j in a] if re.findall(pattern,i)]])
-len([i for i in b.items() if i[1]>1])
-ogartistVerseDf,guestVerseDf = parseCollabs(rg_rap)
+def rg():
+    rg = pd.read_json(rg)
+    #clear out nonrap and nonelnglish songs
+    rg_rap = rg[rg['genre']=='rap'] #get rid of the nonrap
+    rg_rap = rg_rap[rg_rap['year'] != 'N/A'] #clean out undated rap
+    rg_rap['year']=rg_rap['year'].apply(lambda x: x.replace('}','')) #clean it up
+    rg_rap['datetime']=rg_rap['year'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d')) #convert string date to dateime
+    rg_rap['year']=rg_rap['datetime'].apply(lambda x: x.year) #put year in it's own column for simplicity
+    rg_rap = rg_rap[rg_rap['year'] > 1979] #stuff before 1979 isn't rap it's all james brown etc
+    rg_rap['lang'] = rg_rap['lyrics'].apply(lambda x: langDetect(x)) #get english songs
+    rg_rap = rg_rap[rg_rap['lang']=='en'] #banish nonenglish songs
+    #downcase
+    rg_rap['artist'] = rg_rap['artist'].apply(lambda x: x.lower())
+    rg_rap['title'] = rg_rap['title'].apply(lambda x: x.lower())
+    rg_rap['album'] = rg_rap['album'].apply(lambda x: x.lower())
+    rg_rap['artist'] = rg_rap['artist'].apply(lambda x: x.lower())
+    rg_rap['producer'] = rg_rap['producer'].apply(lambda x: [i.lower() for i in x])
+    rg_rap['producer'] = rg_rap['producer'].apply(lambda x: [i.lower() for i in x])
+    rg_rap['featured_artist'] = rg_rap['featured_artist'].apply(lambda x: [i.lower() for i in x])
+    rg_rap['writer'] = rg_rap['writer'].apply(lambda x: [i.lower() for i in x])
+    rg_rap['lyrics'] = rg_rap['lyrics'].apply(lambda x: [i.lower() for i in x])
+    #remove ' lyrics' from song title
+    rg_rap['title'] = rg_rap['title'].apply(lambda x: x.replace(' lyrics',''))
+    #disambiguate featured artists from original songs using parseCollabs function
+    ogartistVerseDf,guestVerseDf = parseCollabs(rg_rap)
+    rg_disam = pd.DataFrame(columns=originalDF.columns)
 
 
+def az():
 
+    az = pd.read_json(az)
+    #filter entries with no year
+    az = az[az.year != 'N/A']
+    #filter songs older than 1975
+    az.year = az.year.astype(int)
+    az = az[az['year'] > 1800]
+    az = az[az['year'] < 2017]
 
-az = pd.read_json(az)
-#filter entries with no year
-az = az[az.year != 'N/A']
-#filter songs older than 1975
-az.year = az.year.astype(int)
-az = az[az['year'] > 1800]
-az = az[az['year'] < 2017]
+    #filter nonrap
+    az_rap = az[az.genre == 'urban']
+    #filter entries with no year
+    az_rap = az_rap[az_rap.year != 'N/A']
+    #filter songs older than 1975
+    az_rap.year = az_rap.year.astype(int)
+    az_rap = az_rap[az_rap['year'] > 1975]
+    az_rap = az_rap[az_rap['year'] < 2020]
+    az_rap = az_rap[az_rap.lyrics != 'N/A']
 
+    '''how to get certain row/column value
+    az_rap['lyrics'].irow(1)
+    another example of a query
+    az_rap[az_rap['artist']=='kanye west']['lyrics'].irow(45)'''
 
+    #lowercase artist names, lyrics, song titles and albums
+    az_rap['artist'] = az_rap['artist'].apply(lambda x: x.lower())
+    az_rap['title'] = az_rap['title'].apply(lambda x: x.lower())
+    az_rap['album'] = az_rap['album'].apply(lambda x: x.lower())
+    az_rap['artist'] = az_rap['artist'].apply(lambda x: x.lower())
+    az_rap['lyrics'] = az_rap['lyrics'].apply(lambda x: [i.lower() for i in x])
 
-#rg_rap = [i for i in rg if i['genre'] == 'rap']
+    pattern = r"_\[.*\]_"
+    #find all possible song breakpoints
+    breaks = az_rap['lyrics'].apply(lambda x: [re.findall(pattern,i) for i in x if re.findall(pattern,i)])
+    breaks2 = flatten([j for j in [i for i in breaks] if j])
+    breaksd = Counter(breaks2)
 
-#filter nonrap
-az_rap = az[az.genre == 'urban']
-#filter entries with no year
-az_rap = az_rap[az_rap.year != 'N/A']
-#filter songs older than 1975
-az_rap.year = az_rap.year.astype(int)
-az_rap = az_rap[az_rap['year'] > 1975]
-az_rap = az_rap[az_rap['year'] < 2020]
-az_rap = az_rap[az_rap.lyrics != 'N/A']
-
-'''how to get certain row/column value
-az_rap['lyrics'].irow(1)
-another example of a query
-az_rap[az_rap['artist']=='kanye west']['lyrics'].irow(45)'''
-
-#lowercase artist names, lyrics, song titles and albums
-az_rap['artist'] = az_rap['artist'].apply(lambda x: x.lower())
-az_rap['title'] = az_rap['title'].apply(lambda x: x.lower())
-az_rap['album'] = az_rap['album'].apply(lambda x: x.lower())
-az_rap['artist'] = az_rap['artist'].apply(lambda x: x.lower())
-az_rap['lyrics'] = az_rap['lyrics'].apply(lambda x: [i.lower() for i in x])
-
-pattern = r"_\[.*\]_"
-#find all possible song breakpoints
-breaks = az_rap['lyrics'].apply(lambda x: [re.findall(pattern,i) for i in x if re.findall(pattern,i)])
-breaks2 = flatten([j for j in [i for i in breaks] if j])
-breaksd = Counter(breaks2)
-
-breaksd.most_common(20)
+    breaksd.most_common(20)
 
 def disambiguateCollabs():
     #check for pattern
@@ -144,9 +131,9 @@ def parseCollabs(originalDF):
     ogartistVerseDf = pd.DataFrame(columns=originalDF.columns)
     featCount=[]
 
-    for row in xrange((20)): #go through the dataframe
+    for row in xrange(len(originalDF)): #go through the dataframe
         curEntry = originalDF.iloc[row] #
-       
+        print row
         lyrics = curEntry['lyrics']
         lyricsOG = lyrics
         featured = curEntry['featured_artist']
@@ -162,7 +149,7 @@ def parseCollabs(originalDF):
                         try:
                             count=i+1; #get the first line of the actual guest verse (first one is just "verse 1: ARTIST")
                             k=lyrics[count]                    
-                            while k != '  ':
+                            while k != '  ': #guest verse always ends with '  '
                                 featCount.append(count)
                                 #del ogartistVerseDf.iloc[row]['lyrics'][count] #delete the featured line from the original song
                                 featVerse.append(k)
@@ -174,7 +161,8 @@ def parseCollabs(originalDF):
                             guestVerseDf = guestVerseDf.append(featEntry)
                         except:
                             pass
-        lyricsOG = [lyrics[i] for i in xrange(len(lyrics)) if i not in featCount]
+        #remove featured lyrics from the original song
+        lyricsOG = [lyrics[i] for i in xrange(len(lyrics)) if i not in featCount] 
         curEntry['lyrics'] = lyricsOG
         ogartistVerseDf = ogartistVerseDf.append(curEntry.copy(deep=True))
 
